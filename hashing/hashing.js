5dfbd538ff337da2d993609d94a585af6b996bd3
Aniket-Bhat <69003723+Aniket-Bhat@users.noreply.github.com>
2021-09-14 23:25:19 +0530

    CodeSandbox URL at https://codesandbox.io/s/aniket-bhat-bitcoin-whitepaper-exercises-hashing-rkbuh?file=/src/index.js

--- hashing/hashing.js
+++ hashing/hashing.js
@@ -1,43 +1,101 @@
-"use strict";
-
 var crypto = require("crypto");
 
-// The Power of a Smile
-// by Tupac Shakur
 var poem = [
-	"The power of a gun can kill",
-	"and the power of fire can burn",
-	"the power of wind can chill",
-	"and the power of a mind can learn",
-	"the power of anger can rage",
-	"inside until it tears u apart",
-	"but the power of a smile",
-	"especially yours can heal a frozen heart",
+  "The power of a gun can kill",
+  "and the power of fire can burn",
+  "the power of wind can chill",
+  "and the power of a mind can learn",
+  "the power of anger can rage",
+  "inside until it tears u apart",
+  "but the power of a smile",
+  "especially yours can heal a frozen heart"
 ];
 
 var Blockchain = {
-	blocks: [],
+  blocks: []
 };
 
-// Genesis block
 Blockchain.blocks.push({
-	index: 0,
-	hash: "000000",
-	data: "",
-	timestamp: Date.now(),
+  index: 0,
+  hash: "000000",
+  data: "",
+  timestamp: Date.now()
 });
 
-// TODO: insert each line into blockchain
-// for (let line of poem) {
-// }
-
-// console.log(`Blockchain is valid: ${verifyChain(Blockchain)}`);
-
-
-// **********************************
-
 function blockHash(bl) {
-	return crypto.createHash("sha256").update(
-		// TODO: use block data to calculate hash
-	).digest("hex");
+  return crypto.createHash("sha256").update(JSON.stringify(bl)).digest("hex");
 }
+
+function createBlock(data) {
+  let height = Blockchain.blocks.length;
+  let block = {
+    index: height,
+    data: data,
+    timestamp: Date.now(),
+    prevHash: Blockchain.blocks[height - 1].hash
+  };
+
+  let hash = blockHash(block);
+
+  block.hash = hash;
+  return block;
+}
+
+function isEmpty(value) {
+  if (value === "" || value === null || value === undefined) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isGenesisBlock(block) {
+  if (block.index === 0 && block.hash === "000000") {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+async function verifyBlock(chain) {
+  return Promise.all(
+    chain.map((block, index, blocks) => {
+      let { hash, ...rest } = block;
+      // console.log(hash)
+      // console.log(block.index > 0)
+      // console.log((isGenesisBlock(block) || !isEmpty(block.prevHash)))
+      // console.log((!isEmpty(block.data) || isGenesisBlock(block)))
+      // console.log(blockHash(rest) === hash || isGenesisBlock(block))
+      if (
+        (!isEmpty(block.data) || isGenesisBlock(block)) &&
+        (isGenesisBlock(block) || !isEmpty(block.prevHash)) &&
+        block.index >= 0 &&
+        (blockHash(rest) === hash || isGenesisBlock(block))
+      ) {
+        if (
+          block.index !== 0 &&
+          block.prevHash === blocks[index - 1].hash
+        ) {
+          return(true);
+        }
+        return(true);
+      } else {
+        console.log(`block ${block.index} is invalid`)
+        return(false);
+      }
+    })
+  );
+}
+
+for (let line of poem) {
+  Blockchain.blocks.push(createBlock(line));
+}
+async function getVerification() {
+  if (await verifyBlock(Blockchain.blocks)) {
+    console.log("All blocks verified");
+  } else {
+    console.log("invalid");
+  }
+}
+
+getVerification();
